d86686c4ab02f778f33159ba948023d19ef79f3e
henry-mahar <henry.mahar@gmail.com>
2017-02-18 15:28:11 -0600

    implemented attemptParse in TimetableParser

--- src/com/yrnehraham/ETime/gfx/ETimePanel.java
+++ src/com/yrnehraham/ETime/gfx/ETimePanel.java
@@ -1,5 +1,6 @@
 package com.yrnehraham.ETime.gfx;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -9,6 +10,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import com.yrnehraham.ETime.parser.TimetableParser;
@@ -22,23 +24,37 @@ public class ETimePanel extends JPanel{
     private int height;
     private JButton enter;
     private JLabel instructions;
-    private JTextField username;
-    private JTextField password;
+    private JTextArea username = new JTextArea("username");
+    private JTextArea password = new JTextArea("password");
     
     public ETimePanel(TimetableParser parser) {
-    	this.width = 200;
-    	this.height = 100;
+
+    	this.width = 400;
+    	this.height = 500;
     	this.enter = new JButton("Start");
     	this.instructions = new JLabel("Test");
     	this.setPreferredSize(new Dimension(width, height));
     	this.setBackground(Color.gray);
-    	this.add(enter);
+    	this.add(enter, BorderLayout.SOUTH);
     	this.add(instructions);
+    	this.username.setEditable(true);
+    	this.password.setEditable(true);
+    	this.username.setBackground(Color.BLUE);
+    	this.username.setBackground(Color.WHITE);
+    	//this.setOpaque(true);
+    	
+    	this.add(username, BorderLayout.CENTER);
+    	this.add(password, BorderLayout.CENTER);
+    	
     	this.enter.addActionListener(new ActionListener() {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				if (!username.getText().trim().equals("") && !password.getText().trim().equals("") && parser != null) {
+					parser.attemptParse(username.getText().trim(), password.getText().trim());
+					parser.printTimes();
+				} else if (parser == null) {
+					throw new NullPointerException("Parser is null");
+				}
 			}});
     }
 }
--- src/com/yrnehraham/ETime/parser/TimetableParser.java
+++ src/com/yrnehraham/ETime/parser/TimetableParser.java
@@ -1,24 +1,46 @@
 package com.yrnehraham.ETime.parser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
 
 public class TimetableParser {
-	
+
 	private Document doc;
-	
+	private ArrayList<String> startTimes = new ArrayList<>();
+	private ArrayList<String> endTimes = new ArrayList<>();
+
 	public TimetableParser() {
-		
+
 	}
-	
-	public void attemptParse(String doc, String pass, String user) {
+
+	public void attemptParse(String user, String pass) {
 		try {
-			this.doc = Jsoup.connect(doc).data("login_username", user, "login_password", pass).post();
+			this.doc = Jsoup.connect("http://tcdb/timesheet.php").data("login_username", user, "login_password", pass).post();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		//System.out.println(doc.toString());
+		Elements timeTable = this.doc.getElementsByClass("ucdb");
+		Elements rows = timeTable.get(0).select("tr");
+		
+		
+		for(int i = 1; i < rows.size(); i += 1) {
+			Element row = rows.get(i);
+		    Elements cols = row.select("td");
+		    if(cols.size() > 4) {
+		    	startTimes.add(cols.get(2).toString());
+			    endTimes.add(cols.get(3).toString());
+		    }
+		}
+		
+	}
+
+	public void printTimes() {
+		for (int i = 0; i < startTimes.size(); i++)
+			System.out.println("Start Time " + startTimes.get(i) + " End Time " + endTimes.get(i));
 	}
 }
